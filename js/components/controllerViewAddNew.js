46b78169987b9eaec962f43d7b2300bc006b8c14
saumya <2saumyaray@gmail.com>
2016-02-04 00:07:48 +0530

    Done with the form

--- js/components/controllerViewAddNew.js
+++ js/components/controllerViewAddNew.js
@@ -4,7 +4,11 @@ var AddNew = React.createClass({
   render: function(){
     //
     return (
-      <div>Add New</div>
+      <div class="twelve columns">
+      	<label for="newAddress">Note</label>
+  			<textarea className="u-full-width" placeholder="New note ..." id="newAddress"></textarea>
+  			<input className="button-primary" type="button" value="Save"></input>
+      </div>
     );
   }
 });
