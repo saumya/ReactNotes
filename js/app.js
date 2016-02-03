e8c6343b7cafee8a285e3bdee6aa0e1b18b2b4b6
Pivotal <Suresh@Sureshs-Mac-mini.local>
2016-02-03 18:16:34 +0530

    started with the implementation

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//
+(function(){
+  console.log('The Address : 4.0.0');
+  var React = require('react');
+  var ReactDOM = require('react-dom');
+  var App = ReactDOM.render(<div>Hello World!</div>,document.getElementById('react-app'));
+})();
