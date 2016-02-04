f0782e0d1a414f690959bef65a045c8b2db6c860
saumya <2saumyaray@gmail.com>
2016-02-04 09:25:50 +0530

    Code cleanup

--- js/components/controllerViewAddNew.js
+++ js/components/controllerViewAddNew.js
@@ -1,11 +1,14 @@
 //
 var React = require('react');
+var Lawnchair = require('../libs/Lawnchair')
+
 var AddNew = React.createClass({
   render: function(){
+    //console.log('AddNew : Lawnchair',Lawnchair);
     //
     return (
-      <div class="twelve columns">
-      	<label for="newAddress">Note</label>
+      <div className="twelve columns">
+      	<label>Note</label>
   			<textarea className="u-full-width" placeholder="New note ..." id="newAddress"></textarea>
   			<input className="button-primary" type="button" value="Save"></input>
       </div>
